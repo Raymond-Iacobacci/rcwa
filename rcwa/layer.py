bd984cef34d5f8cb59cc1e6009544b5166d8ed12
Raymond-Iacobacci <66854622+Raymond-Iacobacci@users.noreply.github.com>
2024-08-13 21:48:38 -0700

    Update layer.py

--- rcwa/layer.py
+++ rcwa/layer.py
@@ -72,10 +72,12 @@ class Layer(MatrixCalculator):
 
     def set_convolution_matrices(self, n_harmonics: Union[ArrayLike, int]):
         if self.crystal is not None:
+            print("CRYSTAL LAYER")
             self.er = self._convolution_matrix(self.crystal.permittivityCellData, n_harmonics)
             self.ur = self._convolution_matrix(self.crystal.permeabilityCellData, n_harmonics)
         else:
-            self.er = self.er * complexIdentity(prod(n_harmonics))
+            print("NONCRYSTAL LAYER")
+            self.er = self.er * complexIdentity(prod(n_harmonics)) # First occurrence of breaking. Replace this function with a gradient-compatible function, or at least change it so that the gradient propagates.
             self.ur = self.ur * complexIdentity(prod(n_harmonics))
 
     def _convolution_matrix(self, cellData: ArrayLike, n_harmonics: Union[ArrayLike, int]) -> ArrayLike:
@@ -85,38 +87,45 @@ class Layer(MatrixCalculator):
             n_harmonics = (n_harmonics,)
 
         if dimension == 1:
-            n_harmonics = (n_harmonics + (1, 1))
+            print("First dimension")
+            # n_harmonics = (n_harmonics + (1, 1))
+            n_harmonics = (1, n_harmonics[0], 1) # fix for extra
+            print(n_harmonics)
         elif dimension == 2:
+            print("Second dimension")
             n_harmonics = (n_harmonics + (1,))
-
+        print("Finished?")
         (P, Q, R) = n_harmonics
-
+        print("Not that")
         convolutionMatrixSize = P*Q*R;
         convolutionMatrixShape = (convolutionMatrixSize, convolutionMatrixSize);
         convolutionMatrix = complexZeros(convolutionMatrixShape)
-
+        print(f"This is the shape before reshaping: {cellData.shape}")
         cellData = reshapeLowDimensionalData(cellData);
         (Nx, Ny, Nz) = cellData.shape;
         zeroHarmonicsLocation = np.array([math.floor(Nx/2), math.floor(Ny/2), math.floor(Nz/2)])
-
+        print(f"Shape going into fast fourier transform: {cellData.shape}")
         cellFourierRepresentation = fftn(cellData);
+        print(f"Shape coming out of fast fourier transform: {cellFourierRepresentation.shape}")
         for rrow in range(R):
             for qrow in range(Q):
                 for prow in range(P):
                     row = rrow*Q*P + qrow*P + prow;
-                    for rcol in range(R):
-                        for qcol in range(Q):
-                            for pcol in range(P):
+                    for rcol in range(rrow, R):
+                        for qcol in range(qrow, Q):
+                            for pcol in range(prow, P):
                                 col = rcol*Q*P + qcol*P + pcol;
                                 # Get the desired harmonics relative to the 0th-order harmonic.
-                                desiredHarmonics = np.array([prow - pcol, qrow - qcol, rrow - rcol])
+                                desiredHarmonics = -np.array([prow - pcol, qrow - qcol, rrow - rcol])
 
                                 # Get those harmonic locations from the zero harmonic location.
                                 desiredHarmonicsLocation = zeroHarmonicsLocation + desiredHarmonics
-
-                                convolutionMatrix[row][col] = \
-                                    cellFourierRepresentation[desiredHarmonicsLocation[0],
-                                    desiredHarmonicsLocation[1], desiredHarmonicsLocation[2]];
+                                print("This is the desiredHarmonic Location:", desiredHarmonicsLocation)
+                                print(cellFourierRepresentation.shape)
+                                # update the convolutionmatrix to match this description of the filters
+                                convolutionMatrix[row][col] = cellFourierRepresentation[desiredHarmonicsLocation[0],desiredHarmonicsLocation[1], desiredHarmonicsLocation[2]];
+                                print("Gotten the locations")
+        print("No worries!")
         if convolutionMatrix.shape == (1, 1):
             convolutionMatrix = convolutionMatrix[0][0]
         return convolutionMatrix;
@@ -217,8 +226,11 @@ class LayerStack:
         self.gapLayer.thickness = 0
         if self._k_dimension == 1:
             self.gapLayer.er = 1 + sq(self.Kx) + sq(self.Ky)
-            self.gapLayer.ur = 1
+            self.gapLayer.ur = 1 + sq(self.Kx) + sq(self.Ky)
+            self.gapLayer.ur = torch.ones(size = self.Kx.shape)
+            
             Qg = self.gapLayer.Q_matrix()
+
             lambda_gap = self.gapLayer.lambda_matrix()
 
         else:
